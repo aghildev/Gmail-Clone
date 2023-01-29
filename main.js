29b5330646b69face21a819eba926327b3db796f
Aghil P Wilson <aghilpwilson1994@gmail.com>
2023-01-29 22:43:12 +0530

    done with Send mail features

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const userEmail = new URL(window.location.href).searchParams.get('email');
 console.log(userEmail);
+const inboxBtn = document.querySelector(".inboxC")
+inboxBtn.addEventListener('click', () => {
+  document.querySelector("#listall").classList.remove("active")
+  inboxBtn.classList.add("active");
+  document.querySelector(".content2").classList.add("displaynone")
+  document.querySelector(".content").style.display = "block";
+})
 
 function showModal() {
   document.querySelector('.sendMail').style.display = 'flex';
@@ -16,6 +23,7 @@ closeButton.addEventListener("click", function () {
 });
 const sendEmail = async (e) => {
   e.preventDefault();
+  document.querySelector('.sendMail').style.display = "none";
   let email = document.getElementById("to").value;
   let senderEmail = userEmail;
   let subject = document.getElementById("subject").value;
@@ -39,7 +47,9 @@ const sendEmail = async (e) => {
 document.querySelector('#sForm').addEventListener('submit', sendEmail)
 const listDetails = async (e) => {
   e.preventDefault();
-
+  document.querySelector(".content").style.display = "none"
+  document.querySelector("#listall").classList.add("active")
+  inboxBtn.classList.remove("active");
   let email = userEmail;
 
   try {
@@ -51,24 +61,168 @@ const listDetails = async (e) => {
       body: JSON.stringify({ email })
     });
     const data = await response.json();
-    let emailData = data.emailData;
+
+    let emailData = data.emailData[0].emailData;
     let emails = [];
 
+    try {
+      emailData = JSON.parse(emailData);
+    } catch (e) {
+      console.error("Invalid JSON string:", emailData);
+      return;
+    }
+
     emailData.forEach((data) => {
-      let emailData = JSON.parse(data.emailData);
-      emails = emails.concat(emailData);
+      if (typeof data === 'string') {
+        try {
+          data = JSON.parse(data);
+        } catch (e) {
+          console.error("Invalid JSON string:", data);
+          return;
+        }
+      }
+      emails.push(data);
     });
 
     const emailTemplate = emails.map((email) => {
+      const currentTime = new Date();
+      const hours = currentTime.getHours();
+      let ampm = 'AM';
+      if (hours >= 12) {
+        ampm = 'PM';
+      }
+      const currentHour = hours % 12 || 12;
+      const currentMinute = currentTime.getMinutes();
       return `
-    <div class="email-item">
-      <h2>Subject: ${email.subject}</h2>
-      <p>Email: ${email.email}</p>
-      <p>Message: ${email.html}</p>
+    
+    
+    <div class="content2">
+    <div class="mail">
+
+      <div class="inbox-message-item">
+
+        <div class="checkbox" style="margin-right: -12px;">
+          <button class="btn">
+            <img src="/app/assets/icons/check_box_outline_blank_black_24dp.svg" alt="Select"
+              class="btn-icon-sm btn-icon-alt btn-icon-hover message-btn-icon">
+          </button>
+        </div>
+
+        <div class="message-group-hidden">
+          <button class="btn-alt btn-nofill drag-indicator">
+            <img src="/app/assets/icons/drag_indicator_black_24dp.svg" alt="Drag"
+              class="btn-icon-sm btn-icon-alt btn-icon-disabled">
+          </button>
+        </div>
+
+        <button class="btn star" style="margin: 0;">
+          <img src="/app/assets/icons/star_border_black_24dp.svg" alt="Not starred"
+            class="btn-icon-sm btn-icon-alt btn-icon-hover message-btn-icon">
+        </button>
+
+        <div class="message-default">
+
+          <div class="message-sender message-content unread">
+            <span>${email.email}</span>
+          </div>
+
+          <div class="message-subject message-content unread">
+            <span>${email.subject}</span>
+          </div>
+
+          <div class="message-seperator message-content"> - </div>
+
+          <div class="message-body message-content">
+            <span> ${email.html}</span>
+          </div>
+
+          <div class="gap message-content"> &nbsp;&nbsp; </div>
+
+          <div class="message-date center-text unread">
+            <span>${currentHour}:${currentMinute} ${ampm}</span>
+          </div>
+
+        </div>
+
+        <div class="message-group-hidden">
+          <div class="inbox-message-item-options">
+            <button class="btn">
+              <img src="/app/assets/icons/archive_black_24dp.svg" alt="Archive"
+                class="btn-icon-sm btn-icon-alt btn-icon-hover">
+            </button>
+
+            <button class="btn">
+              <img src="/app/assets/icons/delete_black_24dp.svg" alt="Delete"
+                class="btn-icon-sm btn-icon-alt btn-icon-hover">
+            </button>
+
+            <button class="btn">
+              <img src="/app/assets/icons/mark_as_unread_black_24dp.svg" alt="Mark as unread"
+                class="btn-icon-sm btn-icon-alt btn-icon-hover">
+            </button>
+
+            <button class="btn">
+              <img src="/app/assets/icons/access_time_filled_black_24dp.svg" alt="Snooze"
+                class="btn-icon-sm btn-icon-alt btn-icon-hover">
+            </button>
+          </div>
+        </div>
+
+      </div>
+
+      
     </div>
+
+    <!-- FOOTER -->
+
+    <footer class="activity">
+      <div class="footer-container">
+
+        <div class="footer-item">
+          <span class="progressbar"></span>
+          <a href="#" class="footer-link footer-text-sm">
+
+
+            <div class="footer-group">
+              <span> 0 GB of 15 GB used</span>
+              <span>
+                <img style="height: 18px; width: 18px; margin: 0 8px; padding: 0;"
+                  src="/app/assets/icons/open_in_new_black_24dp.svg" alt="Google drive storage"
+                  class="btn-icon-alt btn-icon-sm">
+              </span>
+            </div>
+
+          </a>
+        </div>
+
+        <div class="footer-item">
+          <a href="#" class="footer-link footer-text-sm">Terms</a>
+          ·
+          <a href="#" class="footer-link footer-text-sm">Privacy</a>
+          ·
+          <a href="#" class="footer-link footer-text-sm">Program Policies</a>
+        </div>
+
+        <div class="footer-item">
+          <span class="footer-text-sm">Last activity: 37 minutes ago</span>
+
+          <div class="footer-group">
+            <a href="#" class="footer-link footer-text-sm">Details</a>
+          </div>
+        </div>
+
+      </div>
+    </footer>
+
+ 
+    
   `
     }).join('');
-    //document.querySelector('#email-list').innerHTML = emailTemplate;
+
+    //document.querySelector(".content2").innerHTML = emailTemplate;
+    document.querySelector(".content2").insertAdjacentHTML('afterbegin', emailTemplate);
+    document.querySelector(".content2").classList.remove("displaynone");
+    //document.querySelector('#email-list').innerHTML = ;
     console.log(emailTemplate)
     console.log(data);
     console.log("got list")
@@ -76,7 +230,6 @@ const listDetails = async (e) => {
     console.log('Error:', error);
   }
 }
-
 
 document.querySelector("#listall").addEventListener("click", listDetails)
 
@@ -90,4 +243,8 @@ googleCalender.addEventListener('click', function () {
 const googleKeep = document.getElementById("googleKeep");
 googleKeep.addEventListener('click', function () {
   window.location.href = "./googlenotes/notes.html";
+})
+
+document.querySelector(".logoutBtn").addEventListener('click', function () {
+  window.location.href = "./index.html"
 })
